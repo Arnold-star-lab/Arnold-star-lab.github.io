64713a32f6353ebbd6ad2ba26b37d321234899ca
Arnold-star-lab <73660657@continental.edu.pe>
2025-04-01 22:52:53 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+// import siteConfig from './src/data/site-config';  // Esta línea ya no es necesaria
 
 export default defineConfig({
-  site: 'https://Arnold-star-lab.github.io',  // URL de tu sitio en GitHub Pages
+    site: 'https://Arnold-star-lab.github.io',  // Aquí colocas directamente la URL de tu sitio
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [mdx(), sitemap()]
 });
